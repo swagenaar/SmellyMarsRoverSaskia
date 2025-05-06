7d579265e91ac833b0e2a404d8b403881e41369e
Saskia Wagenaar <saskia.wagenaar@ns.nl>
2025-05-06 13:30:31 +0200

    R: Rename rover state variables.

--- src/Rover.ts
+++ src/Rover.ts
@@ -13,9 +13,9 @@ export class Rover {
             const yPosition = splitInitialState[1] ?? defaultPosition;
             const direction = splitInitialState[2] ?? defaultDirection;
 
-            this.roverState.xx = parseInt(xPosition, 10);
-            this.roverState.yy = parseInt(yPosition, 10);
-            this.roverState.dd = direction;
+            this.roverState.x = parseInt(xPosition, 10);
+            this.roverState.y = parseInt(yPosition, 10);
+            this.roverState.direction = direction;
         }
     }
 
@@ -23,44 +23,44 @@ export class Rover {
         for (let i = 0; i < cms.length; i++) {
             const c = cms[i];
             if (c === 'L') {
-                if (this.roverState.dd === 'E') {
-                    this.roverState.dd = 'N';
-                } else if (this.roverState.dd === 'N') {
-                    this.roverState.dd = 'W';
-                } else if (this.roverState.dd === 'W') {
-                    this.roverState.dd = 'S';
-                } else if (this.roverState.dd === 'S') {
-                    this.roverState.dd = 'E';
+                if (this.roverState.direction === 'E') {
+                    this.roverState.direction = 'N';
+                } else if (this.roverState.direction === 'N') {
+                    this.roverState.direction = 'W';
+                } else if (this.roverState.direction === 'W') {
+                    this.roverState.direction = 'S';
+                } else if (this.roverState.direction === 'S') {
+                    this.roverState.direction = 'E';
                 }
             } else if (c === 'R') {
-                if (this.roverState.dd === 'E') {
-                    this.roverState.dd = 'S';
-                } else if (this.roverState.dd === 'S') {
-                    this.roverState.dd = 'W';
-                } else if (this.roverState.dd === 'W') {
-                    this.roverState.dd = 'N';
-                } else if (this.roverState.dd === 'N') {
-                    this.roverState.dd = 'E';
+                if (this.roverState.direction === 'E') {
+                    this.roverState.direction = 'S';
+                } else if (this.roverState.direction === 'S') {
+                    this.roverState.direction = 'W';
+                } else if (this.roverState.direction === 'W') {
+                    this.roverState.direction = 'N';
+                } else if (this.roverState.direction === 'N') {
+                    this.roverState.direction = 'E';
                 }
             } else if (c === 'M') {
-                if (this.roverState.dd === 'E') {
-                    this.roverState.xx++;
+                if (this.roverState.direction === 'E') {
+                    this.roverState.x++;
                 }
-                if (this.roverState.dd === 'S') {
-                    this.roverState.yy--;
+                if (this.roverState.direction === 'S') {
+                    this.roverState.y--;
                 }
-                if (this.roverState.dd === 'W') {
-                    this.roverState.xx--;
+                if (this.roverState.direction === 'W') {
+                    this.roverState.x--;
                 }
-                if (this.roverState.dd === 'N') {
-                    this.roverState.yy++;
+                if (this.roverState.direction === 'N') {
+                    this.roverState.y++;
                 }
             }
         }
     }
 
     public get XYD(): string {
-        return `${this.roverState.xx} ${this.roverState.yy} ${this.roverState.dd}`;
+        return `${this.roverState.x} ${this.roverState.y} ${this.roverState.direction}`;
     }
 
     public pos(): string {
--- src/RoverState.ts
+++ src/RoverState.ts
@@ -1,5 +1,5 @@
 export class RoverState {
-    xx: number = 0;
-    yy: number = 0;
-    dd: string = "N"; // 'char' in C# is effectively a one-character string in TypeScript
-} 
+    x = 0;
+    y = 0;
+    direction = 'N'; // 'char' in C# is effectively a one-character string in TypeScript
+}
