a55bf3620107fd49a046aeddf9c505c2f777138e
Saskia Wagenaar <saskia.wagenaar@ns.nl>
2025-05-06 14:05:50 +0200

    R: Removed unused comment.

--- src/RoverState.ts
+++ src/RoverState.ts
@@ -3,5 +3,5 @@ export type Direction = 'N' | 'E' | 'S' | 'W';
 export class RoverState {
     x = 0;
     y = 0;
-    direction: Direction = 'N'; // 'char' in C# is effectively a one-character string in TypeScript
+    direction: Direction = 'N';
 }
